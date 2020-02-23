50e33b35b8062cd8c4f7761c46eba5b062da8d96
Johannes Petereit <johannes.petereit@adesso.de>
2020-02-23 20:21:31 +0100

    add common project for user tracking with app insights

---
+++ src/BuildingBlocks/ApplicationInsightsExtensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApplicationInsightsExtensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAppInsightsAndTelemetry(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddApplicationInsightsTelemetry(configuration);
+            services.AddApplicationInsightsKubernetesEnricher();
+            services.AddSingleton<ITelemetryInitializer, UserTelemetryInitializer>();
+            return services;
+        }
+    }
+}
---
+++ src/BuildingBlocks/ApplicationInsightsExtensions/UserTelemetryInitializer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ApplicationInsightsExtensions
+{
+    public class UserTelemetryInitializer : ITelemetryInitializer
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ILogger<UserTelemetryInitializer> _logger;
+
+        public UserTelemetryInitializer(IHttpContextAccessor httpContextAccessor, ILogger<UserTelemetryInitializer> logger)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _logger = logger;
+        }
+        public void Initialize(ITelemetry telemetry)
+        {
+            _logger.LogInformation($"called {nameof(UserTelemetryInitializer)}.{nameof(Initialize)}");
+            var ctx = _httpContextAccessor.HttpContext;
+            // If telemetry initializer is called as part of request execution and not from some async thread
+            if (ctx?.User?.Identity != null)
+            {
+                _logger.LogInformation($"Initializing telemetry with user {ctx.User.Identity.Name}");
+                telemetry.Context.User.Id = ctx.User.Identity.Name;
+                telemetry.Context.Session.Id = ctx.User.Identity.Name;
+            }
+        }
+    }
+}
